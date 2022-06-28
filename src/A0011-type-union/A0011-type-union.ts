07fdd62d7565dcc2e02e9468f38eb63d15826f18
Samuel Monteiro <samuelsilva91935@gmail.com>
2022-06-28 17:13:31 -0300

    Tipo union

---
+++ src/A0011-type-union/A0011-type-union.ts
@@ -0,0 +1,10 @@
+// Union types
+function addOrConcat(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(10, 20));
+console.log(addOrConcat('10', '20'));
+console.log(addOrConcat(10, '20'));
+console.log(addOrConcat('10', 20));
